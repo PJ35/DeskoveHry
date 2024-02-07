4584415bf5e78f89a609d896869926374b8c0597
PJ35 <petr.jarot@seznam.cz>
2024-02-07 13:20:40 +0100

    Kód z hodiny 7.2.2024

--- src/DeskovkyFrame.java
+++ src/DeskovkyFrame.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 public class DeskovkyFrame extends JFrame{
     private JPanel mainPanel;
@@ -10,13 +12,16 @@ public class DeskovkyFrame extends JFrame{
     private JRadioButton middleRadioButton;
     private JRadioButton bestRadioButton;
     private JButton previousButton;
+    private JButton saveButton;
     private JButton nextButton;
+    private JButton reloadButton;
     int index = 0;
 
     public DeskovkyFrame() {
         setContentPane(mainPanel);
         setTitle("Deskovky");
-        setBounds(650, 350, 400, 200);
+        setSize(400, 200);
+        setToTheMiddle(this);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         if(Model.getSize() > 0) {
             zobrazDeskovku(index);
@@ -48,6 +53,10 @@ public class DeskovkyFrame extends JFrame{
                 if (index > 0) {
                     index--;
                     zobrazDeskovku(index);
+                } else {
+                    index = Model.getSize() - 1;
+                    zobrazDeskovku(index);
+                    JOptionPane.showMessageDialog(null, "Dosáhli jste začátku seznamu.\nByli jste přesunuti na konec seznamu.");
                 }
             }
         });
@@ -57,7 +66,26 @@ public class DeskovkyFrame extends JFrame{
                 if (index < Model.getSize() - 1) {
                     index++;
                     zobrazDeskovku(index);
+                } else {
+                    index = 0;
+                    zobrazDeskovku(index);
+                    JOptionPane.showMessageDialog(null, "Dosáhli jste konce seznamu.\nByli jste přesunuti na začátek seznamu.");
                 }
+            }
+        });
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Model.setDeskovka(index, new Deskovka(name.getText(), boughtCheckBox.isSelected(), worstRadioButton.isSelected() ? 1
+                        : middleRadioButton.isSelected() ? 2 : bestRadioButton.isSelected() ? 3 : 0));
+                updateFile();
+                JOptionPane.showMessageDialog(null, "Deskovka byla uložena.");
+            }
+        });
+        reloadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                zobrazDeskovku(index);
             }
         });
     }
@@ -67,21 +95,60 @@ public class DeskovkyFrame extends JFrame{
         name.setText(d.getName());
         boughtCheckBox.setSelected(d.isBought());
         switch (d.getRating()) {
-            case 1:
+            case 1 -> {
                 worstRadioButton.setSelected(true);
                 middleRadioButton.setSelected(false);
                 bestRadioButton.setSelected(false);
-                break;
-            case 2:
+            }
+            case 2 -> {
                 worstRadioButton.setSelected(false);
                 middleRadioButton.setSelected(true);
                 bestRadioButton.setSelected(false);
-                break;
-            case 3:
+            }
+            case 3 -> {
                 worstRadioButton.setSelected(false);
                 middleRadioButton.setSelected(false);
                 bestRadioButton.setSelected(true);
-                break;
+            }
+            default -> {
+                worstRadioButton.setSelected(false);
+                middleRadioButton.setSelected(false);
+                bestRadioButton.setSelected(false);
+            }
+        }
+    }
+
+    public void setToTheMiddle(JFrame frame) {
+        GraphicsConfiguration gc = frame.getGraphicsConfiguration();
+        Rectangle bounds = gc.getBounds();
+        Insets screenInsets = Toolkit.getDefaultToolkit().getScreenInsets(gc);
+        Rectangle effectiveScreenArea = new Rectangle();
+
+        effectiveScreenArea.x = bounds.x + screenInsets.left;
+        effectiveScreenArea.y = bounds.y + screenInsets.top;
+        effectiveScreenArea.height = bounds.height - screenInsets.top - screenInsets.bottom;
+        effectiveScreenArea.width = bounds.width - screenInsets.left - screenInsets.right;
+
+        // Center:
+        int middleX = effectiveScreenArea.x + (effectiveScreenArea.width - frame.getWidth()) / 2;
+        int middleY = effectiveScreenArea.y + (effectiveScreenArea.height - frame.getHeight()) / 2;
+        frame.setLocation(middleX, middleY);
+    }
+
+    public void updateFile() {
+        String soubor = "deskovky.txt";
+        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(soubor)))) {
+            for (int i = 0; i < Model.getSize(); i++) {
+                if (i > 3) {
+                    Deskovka d = Model.getDeskovka(i);
+                    writer.println(d.getName() + ";" + d.isBought() + ";" + d.getRating());
+                }
+            }
+            writer.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Soubor "+soubor+" nenalezen!\n"+e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Chyba při zápisu do souboru "+soubor+":\n"+e.getMessage());
         }
     }
 }
--- src/Model.java
+++ src/Model.java
@@ -8,6 +8,7 @@ public class Model {
         seznamDeskovek.add(new Deskovka("Carcassonne", true, 3));
         seznamDeskovek.add(new Deskovka("Catan", false, 2));
         seznamDeskovek.add(new Deskovka("Ticket to Ride", true, 1));
+        seznamDeskovek.add(new Deskovka("Pandemic", true, 3));
     }
 
     public static Deskovka getDeskovka(int index) {
