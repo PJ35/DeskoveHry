cad40406caf58b42055376f7fdff4574b21fe17f
PJ35 <petr.jarot@seznam.cz>
2024-03-04 20:20:27 +0100

    Dokončení kódu z hodiny 28.2.2024

--- src/DeskovkyFrame.java
+++ src/DeskovkyFrame.java
@@ -17,15 +17,6 @@ public class DeskovkyFrame extends JFrame{
     String soubor = "deskovky.txt";
 
     public DeskovkyFrame() {
-        JFileChooser fc = new JFileChooser();
-        fc.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("Textové soubory", "txt"));
-        int result = fc.showOpenDialog(null);
-        if (result == JFileChooser.APPROVE_OPTION) {
-            soubor = fc.getSelectedFile().getAbsolutePath();
-            JOptionPane.showMessageDialog(null, "Byl vybrán soubor "+soubor);
-        } else {
-            JOptionPane.showMessageDialog(null, "Nebyl vybrán žádný soubor.\nByl vybrán výchozí soubor "+soubor);
-        }
         loadFile(soubor);
         setContentPane(mainPanel);
         setTitle("Deskovky");
@@ -65,9 +56,12 @@ public class DeskovkyFrame extends JFrame{
                     index--;
                     zobrazDeskovku(index);
                 } else {
-                    index = Model.getSize() - 1;
-                    zobrazDeskovku(index);
-                    JOptionPane.showMessageDialog(null, "Dosáhli jste začátku seznamu.\nByli jste přesunuti na konec seznamu.");
+                    int result = JOptionPane.showConfirmDialog(null,
+                            "Dosáhli jste začátku seznamu.\nChcete se přesunout na konec seznamu?", "Přesunutí", JOptionPane.YES_NO_OPTION);
+                    if (result == JOptionPane.YES_OPTION) {
+                        index = Model.getSize() - 1;
+                        zobrazDeskovku(index);
+                    }
                 }
             }
         });
@@ -78,25 +72,28 @@ public class DeskovkyFrame extends JFrame{
                     index++;
                     zobrazDeskovku(index);
                 } else {
-                    index = 0;
-                    zobrazDeskovku(index);
-                    JOptionPane.showMessageDialog(null, "Dosáhli jste konce seznamu.\nByli jste přesunuti na začátek seznamu.");
+                    int result = JOptionPane.showConfirmDialog(null,
+                            "Dosáhli jste konce seznamu.\nChcete se přesunout na začátek seznamu?", "Přesunutí", JOptionPane.YES_NO_OPTION);
+                    if (result == JOptionPane.YES_OPTION) {
+                        index = 0;
+                        zobrazDeskovku(index);
+                    }
                 }
             }
         });
     }
 
     private void loadFile(String soubor) {
+        String radek = "";
         try (BufferedReader reader = new BufferedReader(new FileReader(soubor))) {
-            String radek;
             while ((radek = reader.readLine()) != null) {
                 String[] polozky = radek.split(";");
                 Model.addDeskovka(new Deskovka(polozky[0], Boolean.parseBoolean(polozky[1]), Integer.parseInt(polozky[2])));
             }
         } catch (FileNotFoundException e) {
             JOptionPane.showMessageDialog(null, "Soubor "+soubor+" nenalezen!\n"+e.getMessage());
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "Chyba při čtení ze souboru "+soubor+":\n"+e.getMessage());
+        } catch (IOException | NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            JOptionPane.showMessageDialog(null, "Chyba při čtení ze souboru "+soubor+" na řádku "+radek+":\n"+e.getMessage());
         }
     }
 
@@ -120,38 +117,87 @@ public class DeskovkyFrame extends JFrame{
     private void iniMenu() {
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
-        JMenu file = new JMenu("Soubor");
-        menuBar.add(file);
+        JMenu menu = new JMenu("Soubor");
+        menuBar.add(menu);
         JMenuItem menuItem = new JMenuItem("Uložit");
-        file.add(menuItem);
+        menu.add(menuItem);
         menuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Model.setDeskovka(index, new Deskovka(name.getText(), boughtCheckBox.isSelected(), worstRadioButton.isSelected() ? 1
-                        : middleRadioButton.isSelected() ? 2 : bestRadioButton.isSelected() ? 3 : 0));
-                updateFile(soubor);
-                JOptionPane.showMessageDialog(null, "Deskovka byla uložena.");
+                int result = JOptionPane.showConfirmDialog(null, "Opravdu chcete uložit změny?", "Uložit",
+                        JOptionPane.YES_NO_OPTION);
+                if (result == JOptionPane.YES_OPTION) {
+                    Model.setDeskovka(index, new Deskovka(name.getText(), boughtCheckBox.isSelected(), worstRadioButton.isSelected() ? 1
+                            : middleRadioButton.isSelected() ? 2 : bestRadioButton.isSelected() ? 3 : 0));
+                    updateFile(soubor);
+                    JOptionPane.showMessageDialog(null, "Deskovka byla uložena.");
+                }
+            }
+        });
+        menuItem = new JMenuItem("Uložit jako");
+        menu.add(menuItem);
+        menuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fc = new JFileChooser();
+                fc.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("Textové soubory", "txt"));
+                int result = fc.showSaveDialog(null);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    soubor = fc.getSelectedFile().getAbsolutePath();
+                    soubor = soubor.endsWith(".txt") ? soubor : soubor + ".txt";
+                    updateFile(soubor);
+                    JOptionPane.showMessageDialog(null, "Byl vybrán soubor "+soubor);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Nebyl vybrán žádný soubor.\nZůstává vybrán výchozí soubor "+soubor);
+                }
             }
         });
         menuItem = new JMenuItem("Načíst");
-        file.add(menuItem);
+        menu.add(menuItem);
         menuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                zobrazDeskovku(index);
+                JFileChooser fc = new JFileChooser();
+                fc.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("Textové soubory", "txt"));
+                int result = fc.showOpenDialog(null);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    soubor = fc.getSelectedFile().getAbsolutePath();
+                    JOptionPane.showMessageDialog(null, "Byl vybrán soubor "+soubor);
+                    loadFile(soubor);
+                    index = 0;
+                    zobrazDeskovku(index);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Nebyl vybrán žádný soubor.\nZůstává vybrán výchozí soubor "+soubor);
+                }
             }
         });
-        JMenu menu = new JMenu("Akce");
+        menuItem = new JMenuItem("Obnovit");
+        menu.add(menuItem);
+        menuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int result = JOptionPane.showConfirmDialog(null, "Opravdu chcete obnovit stránku?", "Obnovit",
+                        JOptionPane.YES_NO_OPTION);
+                if (result == JOptionPane.YES_OPTION) {
+                    zobrazDeskovku(index);
+                }
+            }
+        });
+        menu = new JMenu("Akce");
         menuBar.add(menu);
         menuItem = new JMenuItem("Přidat");
         menu.add(menuItem);
         menuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Model.addDeskovka(new Deskovka("Nová deskovka", false, 0));
-                index = Model.getSize() - 1;
-                zobrazDeskovku(index);
-                updateFile(soubor);
+                if (Model.getSize() < Integer.MAX_VALUE) {
+                    Model.addDeskovka(new Deskovka("Nová deskovka", false, 0));
+                    index = Model.getSize() - 1;
+                    zobrazDeskovku(index);
+                    updateFile(soubor);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Seznam je plný.");
+                }
             }
         });
         menuItem = new JMenuItem("Odebrat");
@@ -159,17 +205,21 @@ public class DeskovkyFrame extends JFrame{
         menuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Model.removeDeskovka(index);
-                if (index > 0) {
-                    index--;
+                int result = JOptionPane.showConfirmDialog(null, "Opravdu chcete odebrat tuto deskovku?", "Odebrat",
+                        JOptionPane.YES_NO_OPTION);
+                if (result == JOptionPane.YES_OPTION) {
+                    Model.removeDeskovka(index);
+                    if (index > 0) {
+                        index--;
+                    }
+                    if (Model.getSize() > 0) {
+                        zobrazDeskovku(index);
+                    } else {
+                        addBlankPage();
+                        zobrazDeskovku(index);
+                    }
+                    updateFile(soubor);
                 }
-                if (Model.getSize() > 0) {
-                    zobrazDeskovku(index);
-                } else {
-                    addBlankPage();
-                    zobrazDeskovku(index);
-                }
-                updateFile(soubor);
             }
         });
         menuItem = new JMenuItem("Seřadit");
@@ -180,6 +230,28 @@ public class DeskovkyFrame extends JFrame{
                 Model.sort();
                 updateFile(soubor);
                 JOptionPane.showMessageDialog(null, "Seznam byl seřazen.");
+            }
+        });
+        menu = new JMenu("Souhrn");
+        menuBar.add(menu);
+        menuItem = new JMenuItem("Statistika");
+        menu.add(menuItem);
+        menuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int celkem = Model.getSize();
+                StringBuilder sb = new StringBuilder();
+                int zakoupeno = 0;
+                for (int i = 0; i < Model.getSize(); i++) {
+                    if (Model.getDeskovka(i).getRating() == 3) {
+                        sb.append("\n       ").append(Model.getDeskovka(i).getName());
+                    }
+                    if (Model.getDeskovka(i).isBought()) {
+                        zakoupeno++;
+                    }
+                }
+                JOptionPane.showMessageDialog(null, "Počet deskovek: "+celkem+"\nNejoblíbenější deskovky: "+sb+
+                        "\nPočet zakoupených deskovek: "+zakoupeno);
             }
         });
     }
@@ -217,15 +289,15 @@ public class DeskovkyFrame extends JFrame{
     }
 
     public void updateFile(String soubor) {
+        String radek = "";
         try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(soubor)))) {
             for (int i = 0; i < Model.getSize(); i++) {
                 Deskovka d = Model.getDeskovka(i);
-                writer.println(d.getName() + ";" + d.isBought() + ";" + d.getRating());
+                radek = d.getName() + ";" + d.isBought() + ";" + d.getRating();
+                writer.println(radek);
             }
-        } catch (FileNotFoundException e) {
-            JOptionPane.showMessageDialog(null, "Soubor "+soubor+" nenalezen!\n"+e.getMessage());
         } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "Chyba při zápisu do souboru "+soubor+":\n"+e.getMessage());
+            JOptionPane.showMessageDialog(null, "Chyba při zápisu do souboru "+soubor+" na řádku "+radek+":\n"+e.getMessage());
         }
     }
 }
